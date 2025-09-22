c100b3b163557e3a0d3ce6c30fdbc35a9960058f
mooogy <francismigsanjuan@gmail.com>
2025-09-21 18:53:07 -0700

    setup commit

---
+++ apps/rps_client.cpp
@@ -0,0 +1,10 @@
+#include <boost/asio.hpp>
+#include <iostream>
+
+int main() {
+
+  std::cout << "This is the client\n";
+
+
+  return 0;
+}
---
+++ apps/rps_server.cpp
@@ -0,0 +1,9 @@
+#include <boost/asio.hpp>
+#include <iostream>
+
+int main() {
+
+  std::cout << "This is the server\n";
+
+  return 0;
+}
